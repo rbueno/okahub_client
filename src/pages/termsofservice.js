75d49628e0dc2f8ff51879785f553c4d82860c29
Rafael Bueno <rafaelbuenolink@gmail.com>
2022-06-30 14:01:26 -0300

    termos de uso e política de privacidade

---
+++ src/pages/termsofservice.js
@@ -0,0 +1,57 @@
+// @mui
+import { styled } from '@mui/material/styles';
+import { Box, Container } from '@mui/material';
+import Layout from '../layouts';
+// components
+import Page from '../components/Page';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled('div')(({ theme }) => ({
+  minHeight: '100%',
+  paddingTop: theme.spacing(15),
+  paddingBottom: theme.spacing(10),
+}));
+
+// ----------------------------------------------------------------------
+
+TermsOfService.getLayout = function getLayout(page) {
+  return <Layout variant="logoOnly">{page}</Layout>;
+};
+export default function TermsOfService() {
+    return (
+    <Page title='Privacy of Policy'>
+        <RootStyle>
+      <Container>
+      <h2>Termos de Serviço</h2>
+  <p>Ao acessar ao site <a href="https://Okahub.app">Okahub</a>, concorda em cumprir estes termos de servi&ccedil;o, todas as leis e regulamentos aplic&aacute;veis ​​e concorda que &eacute; respons&aacute;vel pelo cumprimento de todas as leis locais aplic&aacute;veis. Se voc&ecirc; n&atilde;o concordar com algum desses termos, est&aacute; proibido de usar ou acessar este site. Os materiais contidos neste site s&atilde;o protegidos pelas leis de direitos autorais e marcas comerciais aplic&aacute;veis.</p>
+  <h2>2. Uso de Licen&ccedil;a</h2>
+  <p>&Eacute; concedida permiss&atilde;o para baixar temporariamente uma c&oacute;pia dos materiais (informa&ccedil;&otilde;es ou software) no site Okahub , apenas para visualiza&ccedil;&atilde;o transit&oacute;ria pessoal e n&atilde;o comercial. Esta &eacute; a concess&atilde;o de uma licen&ccedil;a, n&atilde;o uma transfer&ecirc;ncia de t&iacute;tulo e, sob esta licen&ccedil;a, voc&ecirc; n&atilde;o pode:&nbsp;</p>
+  <ol>
+  <li>modificar ou copiar os materiais;&nbsp;</li>
+  <li>usar os materiais para qualquer finalidade comercial ou para exibi&ccedil;&atilde;o p&uacute;blica (comercial ou n&atilde;o comercial);&nbsp;</li>
+  <li>tentar descompilar ou fazer engenharia reversa de qualquer software contido no site Okahub;&nbsp;</li>
+  <li>remover quaisquer direitos autorais ou outras nota&ccedil;&otilde;es de propriedade dos materiais; ou&nbsp;</li>
+  <li>transferir os materiais para outra pessoa ou 'espelhe' os materiais em qualquer outro servidor.</li>
+  </ol>
+  <p>Esta licen&ccedil;a ser&aacute; automaticamente rescindida se voc&ecirc; violar alguma dessas restri&ccedil;&otilde;es e poder&aacute; ser rescindida por Okahub a qualquer momento. Ao encerrar a visualiza&ccedil;&atilde;o desses materiais ou ap&oacute;s o t&eacute;rmino desta licen&ccedil;a, voc&ecirc; deve apagar todos os materiais baixados em sua posse, seja em formato eletr&oacute;nico ou impresso.</p>
+  <h2>3. Isen&ccedil;&atilde;o de responsabilidade</h2>
+  <ol>
+  <li>Os materiais no site da Okahub s&atilde;o fornecidos 'como est&atilde;o'. Okahub n&atilde;o oferece garantias, expressas ou impl&iacute;citas, e, por este meio, isenta e nega todas as outras garantias, incluindo, sem limita&ccedil;&atilde;o, garantias impl&iacute;citas ou condi&ccedil;&otilde;es de comercializa&ccedil;&atilde;o, adequa&ccedil;&atilde;o a um fim espec&iacute;fico ou n&atilde;o viola&ccedil;&atilde;o de propriedade intelectual ou outra viola&ccedil;&atilde;o de direitos.</li>
+  <li>Al&eacute;m disso, o Okahub n&atilde;o garante ou faz qualquer representa&ccedil;&atilde;o relativa &agrave; precis&atilde;o, aos resultados prov&aacute;veis ​​ou &agrave; confiabilidade do uso dos materiais em seu site ou de outra forma relacionado a esses materiais ou em sites vinculados a este site.</li>
+  </ol>
+  <h2>4. Limita&ccedil;&otilde;es</h2>
+  <p>Em nenhum caso o Okahub ou seus fornecedores ser&atilde;o respons&aacute;veis ​​por quaisquer danos (incluindo, sem limita&ccedil;&atilde;o, danos por perda de dados ou lucro ou devido a interrup&ccedil;&atilde;o dos neg&oacute;cios) decorrentes do uso ou da incapacidade de usar os materiais em Okahub, mesmo que Okahub ou um representante autorizado da Okahub tenha sido notificado oralmente ou por escrito da possibilidade de tais danos. Como algumas jurisdi&ccedil;&otilde;es n&atilde;o permitem limita&ccedil;&otilde;es em garantias impl&iacute;citas, ou limita&ccedil;&otilde;es de responsabilidade por danos conseq&uuml;entes ou incidentais, essas limita&ccedil;&otilde;es podem n&atilde;o se aplicar a voc&ecirc;.</p>
+  <h2>5. Precis&atilde;o dos materiais</h2>
+  <p>Os materiais exibidos no site da Okahub podem incluir erros t&eacute;cnicos, tipogr&aacute;ficos ou fotogr&aacute;ficos. Okahub n&atilde;o garante que qualquer material em seu site seja preciso, completo ou atual. Okahub pode fazer altera&ccedil;&otilde;es nos materiais contidos em seu site a qualquer momento, sem aviso pr&eacute;vio. No entanto, Okahub n&atilde;o se compromete a atualizar os materiais.</p>
+  <h2>6. Links</h2>
+  <p>O Okahub n&atilde;o analisou todos os sites vinculados ao seu site e n&atilde;o &eacute; respons&aacute;vel pelo conte&uacute;do de nenhum site vinculado. A inclus&atilde;o de qualquer link n&atilde;o implica endosso por Okahub do site. O uso de qualquer site vinculado &eacute; por conta e risco do usu&aacute;rio.</p>
+  <h3>Modifica&ccedil;&otilde;es</h3>
+  <p>O Okahub pode revisar estes termos de servi&ccedil;o do site a qualquer momento, sem aviso pr&eacute;vio. Ao usar este site, voc&ecirc; concorda em ficar vinculado &agrave; vers&atilde;o atual desses termos de servi&ccedil;o.</p>
+  <h3>Lei aplic&aacute;vel</h3>
+  <p>Estes termos e condi&ccedil;&otilde;es s&atilde;o regidos e interpretados de acordo com as leis do Okahub e voc&ecirc; se submete irrevogavelmente &agrave; jurisdi&ccedil;&atilde;o exclusiva dos tribunais naquele estado ou localidade.</p>
+</Container>
+</RootStyle>
+</Page>
+    )
+  }
