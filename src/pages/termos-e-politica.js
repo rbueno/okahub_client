7379d58c295f0ed2b0cae051e6c7ffe4e784b70b
rbueno <rafaelbuenolink@gmail.com>
2023-03-27 07:32:12 -0300

    general update

--- src/pages/termos-e-politica.js
+++ src/pages/termos-e-politica.js
@@ -23,29 +23,29 @@ export default function AboutPage() {
 
       <Container>
       <Typography variant="h2" sx={{ mb: 3 }}>
-      Termos de Uso e Política de Privacidade da OkaAI
+      Termos de Uso e Política de Privacidade do Pulse
               </Typography>
             
 
-<Typography>Bem-vindo(a) ao OkaAI, inteligência artificial do Okahub. Leia atentamente estes termos de uso e política de privacidade antes de usar a OkaAI da Okahub.</Typography>
+<Typography>Bem-vindo(a) ao Pulse, inteligência artificial do Okahub. Leia atentamente estes termos de uso e política de privacidade antes de usar o Pulse da Okahub.</Typography>
             
               <h2>1. Aceitação dos Termos de Uso e Política de Privacidade</h2>
-<Typography>Ao usar o OkaAI, você concorda em cumprir e estar sujeito a estes termos de uso e política de privacidade, bem como às leis e regulamentos aplicáveis. Se você não concorda com estes termos, não use o OkaAI.</Typography>
+<Typography>Ao usar o Pulse, você concorda em cumprir e estar sujeito a estes termos de uso e política de privacidade, bem como às leis e regulamentos aplicáveis. Se você não concorda com estes termos, não use o Pulse.</Typography>
 
-<h2>2. Uso do OkaAI</h2>
-<Typography>O OkaAI é fornecido gratuitamente e permite que os usuários interajam com um chatbot. Ao usar o OkaAI, você concorda em não usá-lo de maneira que possa danificar, desabilitar, sobrecarregar ou prejudicar a plataforma.</Typography>
+<h2>2. Uso do Pulse</h2>
+<Typography>O Pulse é fornecido gratuitamente e permite que os usuários interajam com um chatbot. Ao usar o Pulse, você concorda em não usá-lo de maneira que possa danificar, desabilitar, sobrecarregar ou prejudicar a plataforma.</Typography>
 
 <h2>3. Propriedade Intelectual</h2>
-<Typography>Todos os direitos autorais, marcas comerciais, patentes e outros direitos de propriedade intelectual relacionados ao OkaAI pertencem exclusivamente à empresa proprietária do aplicativo. O usuário não tem permissão para reproduzir, distribuir ou criar trabalhos derivados do OkaAI sem autorização prévia por escrito.</Typography>
+<Typography>Todos os direitos autorais, marcas comerciais, patentes e outros direitos de propriedade intelectual relacionados ao Pulse pertencem exclusivamente à empresa proprietária do aplicativo. O usuário não tem permissão para reproduzir, distribuir ou criar trabalhos derivados do Pulse sem autorização prévia por escrito.</Typography>
 
 <h2>4. Isenção de Responsabilidade</h2>
-<Typography>O OkaAI é fornecido como está, sem garantia de qualquer tipo, expressa ou implícita. A empresa proprietária do OkaAI não garante que o aplicativo atenderá às suas necessidades ou que será ininterrupto, seguro ou livre de erros.</Typography>
+<Typography>O Pulse é fornecido como está, sem garantia de qualquer tipo, expressa ou implícita. A empresa proprietária do Pulse não garante que o aplicativo atenderá às suas necessidades ou que será ininterrupto, seguro ou livre de erros.</Typography>
 
 <h2>5. Limitação de Responsabilidade</h2>
-<Typography>Em nenhuma circunstância, a empresa proprietária do OkaAI será responsável por danos diretos, indiretos, punitivos, incidentais, especiais ou consequenciais decorrentes ou relacionados ao uso ou incapacidade de uso do OkaAI.</Typography>
+<Typography>Em nenhuma circunstância, a empresa proprietária do Pulse será responsável por danos diretos, indiretos, punitivos, incidentais, especiais ou consequenciais decorrentes ou relacionados ao uso ou incapacidade de uso do Pulse.</Typography>
 
 <h2>6. Alterações nos Termos de Uso e Política de Privacidade</h2>
-<Typography>A empresa proprietária do OkaAI pode atualizar estes termos de uso e política de privacidade periodicamente para refletir mudanças em nossas práticas de coleta, uso e compartilhamento de informações. Ao continuar a usar o OkaAI após a publicação das alterações, você concorda em cumprir os termos atualizados.</Typography>
+<Typography>A empresa proprietária do Pulse pode atualizar estes termos de uso e política de privacidade periodicamente para refletir mudanças em nossas práticas de coleta, uso e compartilhamento de informações. Ao continuar a usar o Pulse após a publicação das alterações, você concorda em cumprir os termos atualizados.</Typography>
 
 <h2>7. Contato</h2>
 <Typography>Se você tiver alguma dúvida sobre estes termos de uso e política de privacidade, entre em contato conosco pelo e-mail contato@okahub.com.</Typography>
@@ -53,7 +53,7 @@ export default function AboutPage() {
 <h2>8. Lei Aplicável</h2>
 <Typography>Estes termos de uso e política de privacidade serão regidos e interpretados de acordo com as leis brasileiras.</Typography>
 
-<Typography>Ao utilizar o OkaAI, você declara ter lido e compreendido estes termos </Typography>
+<Typography>Ao utilizar o Pulse, você declara ter lido e compreendido estes termos </Typography>
 
       </Container>
       
