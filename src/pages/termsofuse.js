17d238e0a9282d4e43f80ea56d4b7a78327f81b5
rbueno <rafaelbuenolink@gmail.com>
2023-04-08 15:21:14 -0300

    general update

---
+++ src/pages/termsofuse.js
@@ -0,0 +1,134 @@
+// next
+import Head from 'next/head';
+// @mui
+import { Container, Typography } from '@mui/material';
+// layouts
+import MainLayout from '../layouts/main';
+
+// sections
+import { AboutHero, AboutWhat, AboutTeam, AboutVision, AboutTestimonials } from '../sections/about';
+
+// ----------------------------------------------------------------------
+
+AboutPage.getLayout = (page) => <MainLayout>{page}</MainLayout>;
+
+// ----------------------------------------------------------------------
+
+export default function AboutPage() {
+  return (
+    <>
+      <Head>
+        <title>Termos de uso | Okahub</title>
+      </Head>
+
+      <Container>
+	
+
+<main>
+<h1>Termos de uso - Okahub Pulse chatbot</h1>
+<p>Este termo de uso tem como objetivo estabelecer as condições de uso do Pulse chatbot e as responsabilidades da empresa desenvolvedora em relação à proteção dos dados pessoais dos usuários, em conformidade com a Lei Geral de Proteção de Dados (LGPD).</p>
+
+<h2>1. Definições</h2>
+
+<p>Para fins deste termo de uso, considera-se:</p>
+
+<ul>
+	<li><strong>Pulse chatbot:</strong> software desenvolvido pela empresa para interagir com usuários do Instagram.</li>
+	<li><strong>Usuário:</strong> pessoa física ou jurídica que faz uso do Pulse chatbot.</li>
+	<li><strong>Dados pessoais:</strong> informações relacionadas a uma pessoa física identificada ou identificável, como nome, e-mail, telefone, entre outros.</li>
+	<li><strong>Tratamento de dados pessoais:</strong> qualquer operação realizada com dados pessoais, como coleta, armazenamento, uso, compartilhamento, entre outras.</li>
+</ul>
+
+<h2>2. Condições de Uso</h2>
+
+<p>Ao fazer uso do Pulse chatbot, o usuário concorda em cumprir com as seguintes condições:</p>
+
+<ul>
+	<li>O Pulse chatbot é disponibilizado gratuitamente e pode ser utilizado por qualquer pessoa com acesso à plataforma do Instagram.</li>
+	<li>O usuário é o único responsável pelo conteúdo das mensagens enviadas ao Pulse chatbot, bem como pela veracidade e atualização dos dados pessoais fornecidos.</li>
+	<li>A empresa não se responsabiliza por quaisquer danos decorrentes do uso do Pulse chatbot, incluindo perda de dados, interrupção de negócios ou danos à imagem ou reputação do usuário.</li>
+	<li>A empresa se reserva o direito de bloquear o acesso do usuário ao Pulse chatbot caso detecte o uso indevido do serviço.</li>
+</ul>
+
+<h2>3. Coleta e Tratamento de Dados Pessoais</h2>
+
+<p>A empresa se compromete a coletar e tratar os dados pessoais do usuário de forma transparente, lícita, adequada e necessária, em conformidade com os princípios estabelecidos na LGPD.</p>
+
+<p>O Pulse chatbot pode coletar os seguintes dados pessoais do usuário:</p>
+
+<ul>
+	<li>Nome de usuário;</li>
+	<li>E-mail;</li>
+	<li>Telefone;</li>
+</ul>
+
+<p>Os dados pessoais coletados serão utilizados para as seguintes finalidades:</p>
+
+<ul>
+	<li>Identificação e autenticação do usuário;</li>
+	<li>Envio de informações e notificações relacionadas ao Pulse chatbot;</li>
+	<li>Realização de estatísticas e análises de uso do Pulse chatbot;</li>
+	<li>Atendimento e suporte ao usuário;</li>
+	<li>Personalização da experiência do usuário no Pulse chatbot;</li>
+	<li>Envio de comunicações de marketing e publicidade, desde que autorizado pelo usuário.</li>
+</ul>
+
+<p>A coleta e tratamento dos dados pessoais do usuário serão realizados mediante consentimento prévio e específico do usuário, de forma clara e objetiva, em conformidade com os requisitos da LGPD.</p>
+
+<p>O usuário poderá revogar o consentimento a qualquer momento, mediante solicitação à empresa desenvolvedora.</p>
+
+<p>A empresa se compromete a adotar as medidas técnicas e organizacionais adequadas para proteger os dados pessoais do usuário contra acesso não autorizado, perda, alteração, divulgação ou destruição, em conformidade com as normas aplicáveis de segurança da informação.</p>
+
+<h2>4. Compartilhamento de Dados Pessoais</h2>
+
+<p>A empresa poderá compartilhar os dados pessoais do usuário com terceiros, nas seguintes hipóteses:</p>
+
+<ul>
+	<li>Com empresas parceiras que prestem serviços em nome da empresa desenvolvedora;</li>
+	<li>Para cumprimento de obrigações legais, regulatórias ou judiciais;</li>
+	<li>Em caso de fusão, aquisição, incorporação ou venda de ativos da empresa desenvolvedora;</li>
+	<li>Com autorização prévia e específica do usuário.</li>
+</ul>
+
+<p>A empresa se compromete a adotar as medidas necessárias para garantir a proteção dos dados pessoais do usuário no compartilhamento com terceiros.</p>
+
+<h2>5. Direitos do Usuário</h2>
+
+<p>O usuário poderá exercer os seguintes direitos em relação aos seus dados pessoais coletados e tratados pelo Pulse chatbot:</p>
+
+<ul>
+	<li>Acessar os dados pessoais;</li>
+	<li>Corrigir dados incompletos, inexatos ou desatualizados;</li>
+	<li>Eliminar dados pessoais desnecessários ou excessivos;</li>
+	<li>Revogar o consentimento;</li>
+	<li>Solicitar a portabilidade dos dados;</li>
+	<li>Receber informações sobre o compartilhamento de dados pessoais com terceiros;</li>
+	<li>Solicitar a exclusão de dados pessoais tratados com base no consentimento do usuário, exceto nas hipóteses previstas em lei.</li>
+</ul>
+
+<p>O usuário poderá exercer seus direitos mediante solicitação à empresa desenvolvedora, que deverá responder ao pedido em prazo razoável e de acordo com as disposições legais aplicáveis.</p>
+
+<h2>6. Disposições Gerais</h2>
+
+<p>Este termo de uso será regido e interpretado de acordo com as leis brasileiras, sendo eleito o foro da Comarca de São Paulo para dirimir qualquer controvérsia decorrente deste termo de uso, com renúncia expressa a qualquer outro.</p>
+
+<p>A empresa se reserva o direito de modificar este termo de uso a qualquer momento, mediante publicação de versão atualizada em seu website ou por meio de aviso ao usuário. O uso contínuo do Pulse chatbot pelo usuário após a modificaçãodo termo de uso implica na aceitação das novas condições.</p>
+
+<p>Caso alguma disposição deste termo de uso seja considerada inválida ou inexequível, as demais disposições permanecerão em pleno vigor e efeito.</p>
+
+<h2>7. Contato</h2>
+
+<p>Em caso de dúvidas, sugestões ou reclamações sobre o Pulse chatbot ou sobre este termo de uso, o usuário poderá entrar em contato com a empresa desenvolvedora pelo e-mail contact@okahub.com.</p>
+
+<h2>Conclusão</h2>
+
+<p>Ao utilizar o Pulse chatbot, o usuário estará ciente e concordará com as disposições deste termo de uso e com a política de privacidade, garantindo, assim, a segurança e a privacidade de seus dados pessoais. É importante ressaltar que o cumprimento das disposições da LGPD é fundamental para a proteção dos dados pessoais do usuário e a empresa desenvolvedora está comprometida em cumpri-las integralmente.</p>
+
+
+</main>
+
+
+      </Container>
+    </>
+  );
+}
