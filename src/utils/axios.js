7c280fa67d45f7c262af5e96a538f16154b59ca8
rbueno <rafaelbuenolink@gmail.com>
2023-05-07 21:03:21 -0300

    general

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ import { HOST_API_KEY } from '../config-global';
 
 // ----------------------------------------------------------------------
 
-const axiosInstance = axios.create({ baseURL: HOST_API_KEY, timeout: 1000 * 60 });
+const axiosInstance = axios.create({ baseURL: HOST_API_KEY });
 
 axiosInstance.interceptors.response.use(
   (response) => response,
