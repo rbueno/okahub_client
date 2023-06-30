bc2449e991a7511d0043e12d0d16f06450edba6f
rbueno <rafaelbuenolink@gmail.com>
2023-06-30 17:29:09 -0300

    general update

--- src/sections/@dashboard/benchmarkInstagram/listPost/UserTableRow.js
+++ src/sections/@dashboard/benchmarkInstagram/listPost/UserTableRow.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import { format } from 'date-fns'
 // @mui
 import {
   Stack,
@@ -12,8 +13,14 @@ import {
   IconButton,
   Typography,
 } from '@mui/material';
+import DuoIcon from '@mui/icons-material/Duo';
 // components
-
+import Label from '../../../../components/label';
+import Img from '../../../../components/img';
+import Iconify from '../../../../components/iconify';
+import MenuPopover from '../../../../components/menu-popover';
+import ConfirmDialog from '../../../../components/confirm-dialog';
+import { fDate } from '../../../../utils/formatTime';
 
 // ----------------------------------------------------------------------
 
@@ -52,12 +59,17 @@ export default function UserTableRow({ row, selected, onSelectRow, onAnalyticRow
       <TableRow hover selected={selected}>
             
         <TableCell align="left">
-        {rank}
+          <Label
+            variant="soft"
+            color='success'
+          >
+            {rank}
+          </Label>
         </TableCell>
 
         <TableCell>
           <Stack direction="row" alignItems="center" spacing={2}>
-           Image
+          {mediaType !== 'VIDEO' ? <Img src={mediaUrl} sx={{ width: 60, height: 60}} /> : <DuoIcon />}
           </Stack>
         </TableCell>
 
